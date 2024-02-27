8f046a6a5c6224c7389c656ad04b4cf4ef239d45
quyunfei <yunfei2016@foxmail.com>
2024-02-27 19:49:18 +0800

    update 增加了跳过带密码的压缩包

--- unzip_all_files.py
+++ unzip_all_files.py
@@ -1,27 +1,36 @@
 import os
 import zipfile
 import rarfile
-
+import send2trash
 
 def extract_files_in_directory(directory):
     # 获取当前目录下的所有文件
     files = os.listdir(directory)
-
+    
     for file in files:
         file_path = os.path.join(directory, file)
         if zipfile.is_zipfile(file_path):
             # 如果是zip文件，解压缩
             with zipfile.ZipFile(file_path, 'r') as zip_ref:
-                zip_ref.extractall(directory)
-            print(f"解压缩 {file} 完成")
+                try:
+                    zip_ref.extractall(directory)
+                    print(f"解压缩 {file} 完成")
+                    send2trash.send2trash(file_path)  # 将文件移动到回收站
+                    print(f"已将 {file} 移动到回收站")
+                except RuntimeError:
+                    print(f"文件 {file} 需要密码，跳过解压")
         elif rarfile.is_rarfile(file_path):
             # 如果是rar文件，解压缩
             with rarfile.RarFile(file_path, 'r') as rar_ref:
-                rar_ref.extractall(directory)
-            print(f"解压缩 {file} 完成")
+                try:
+                    rar_ref.extractall(directory)
+                    print(f"解压缩 {file} 完成")
+                    send2trash.send2trash(file_path)  # 将文件移动到回收站
+                    print(f"已将 {file} 移动到回收站")
+                except rarfile.PasswordRequired:
+                    print(f"文件 {file} 需要密码，跳过解压")
         else:
             print(f"文件 {file} 不是压缩文件，跳过")
-
 
 if __name__ == "__main__":
     current_directory = os.getcwd()
